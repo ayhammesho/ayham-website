080b77ad4a44a4ddd120235da6efb5e004503b0f
ayhmmesho <89388392+ayhammesho@users.noreply.github.com>
2023-10-31 14:45:03 +0200

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
-"use client";
-import React, { useState, useEffect } from "react";
 import "./globals.css";
 
 import { Inter } from "next/font/google";
-import { usePathname } from "next/navigation";
-import Loader from "@/components/ui/Loader";
 
 import { Providers } from "./providers";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  //other meta data key values here...
+  title: "Ayham Mesho",
+  description:
+    "Hi my name is Ayham Mesho I’m software engineer specializing in building (and occasionally designing) exceptional digital experiences.",
+  openGraph: {
+    title: "Ayham Mesho",
+    description:
+      "Hi my name is Ayham Mesho I’m software engineer specializing in building (and occasionally designing) exceptional digital experiences.",
+  },
+};
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -14,16 +23,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isHome = pathname === "/";
-  const [isLoading, setIsLoading] = useState(isHome);
-
-  useEffect(() => {
-    if (isLoading) {
-      return;
-    }
-  }, [isLoading]);
-
   return (
     <html lang="en">
       <head>
@@ -50,11 +49,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>
-        {isLoading ? (
-          <Loader finishLoading={() => setIsLoading(false)} />
-        ) : (
-          <Providers>{children}</Providers>
-        )}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "@/components/layout/Header";
 import About from "@/components/sections/About";
 import Jobs from "@/components/sections/Jobs";
@@ -13,7 +13,20 @@ import SocialMedia from "@/components/ui/SocialMedia";
 import Email from "@/components/ui/Email";
 import Lenis from "@studio-freight/lenis";
 
+import { usePathname } from "next/navigation";
+import Loader from "@/components/ui/Loader";
+
 export default function Home() {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+  const [isLoading, setIsLoading] = useState(isHome);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+  }, [isLoading]);
+
   useEffect(() => {
     const lenis = new Lenis();
 
@@ -27,16 +40,22 @@ export default function Home() {
   }, []);
   return (
     <>
-      <Email />
-      <SocialMedia />
-      <Header />
-      <About />
-      <Jobs />
-      <FeaturedWork />
-      <ArchiveProjects />
-      <Recommendation />
-      <Clients />
-      <Contact />
+      {isLoading ? (
+        <Loader finishLoading={() => setIsLoading(false)} />
+      ) : (
+        <>
+          <Email />
+          <SocialMedia />
+          <Header />
+          <About />
+          <Jobs />
+          <FeaturedWork />
+          <ArchiveProjects />
+          <Recommendation />
+          <Clients />
+          <Contact />{" "}
+        </>
+      )}
     </>
   );
 }
